077a26b2490b7ded1c3107adf94102135491d3c1
pavan-sachi <pavan.sachidananda@siteminder.com>
2024-11-19 23:10:50 +1100

    clients connecting to dashboard

--- server.js
+++ server.js
@@ -12,22 +12,34 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    console.log('A user connected', socket.id);
+
+    // socket for dashboard
+    socket.on('join_dashboard', () => {
+        console.log('User joined dashboard', socket.id);
+        // Join the dashboard room
+        socket.join('dashboard');
+    });
+
+    // socket for client
+    socket.on('client_joined', () => {
+        console.log('client joined', socket.id);
+        io.to('dashboard').emit('new_client', socket.id);
+    });
 
     // Emit the list of connected clients to the newly connected client
-    const connectedClients = Array.from(io.sockets.sockets.keys());
-    io.emit('client-list', connectedClients);
+    // const connectedClients = Array.from(io.sockets.sockets.keys());
+    // io.emit('client-list', connectedClients);
 
     socket.on('disconnect', () => {
         console.log('User disconnected', socket.id);
-        const updatedClients = Array.from(io.sockets.sockets.keys());
-        socket.broadcast.emit('client-list', updatedClients);
+        // const updatedClients = Array.from(io.sockets.sockets.keys());
+        // socket.broadcast.emit('client-list', updatedClients);
     });
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
 
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
